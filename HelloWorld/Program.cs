363d498181969ad9d12bd5c588796c7ebafc634e
KarsonAlford <karson.alford@gmail.com>
2022-05-15 16:36:32 -0500

    Minimal ManagedIdentity test

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
 
 namespace HelloWorld
 {
@@ -21,6 +23,26 @@ namespace HelloWorld
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var configRoot = config.Build();
+                    var endpoint = configRoot.GetSection("AppConfigurationSettings").GetValue<string>("Endpoint");
+                    config.AddAzureAppConfiguration(options =>
+                    {
+                        var credential = new DefaultAzureCredential();
+                        options.Connect(new Uri(endpoint), credential)
+                            // Load configuration values with no label
+                            .Select(KeyFilter.Any, LabelFilter.Null)
+                            // Override with any configuration values specific to current hosting env
+                            .Select(KeyFilter.Any, hostingContext.HostingEnvironment.EnvironmentName);
+                        // options.ConfigureRefresh(configure =>
+                        // {
+                        //     configure.Register("refreshSettings", true);
+                        //     configure.SetCacheExpiration(TimeSpan.FromMinutes(10));
+                        // });
+                    });
+                    config.Build();
                 });
     }
 }
